709136ef31034d69f7605753899f63a2b78cb3f8
yoshimaru46 <yshr446@gmail.com>
2022-01-08 18:43:09 +0900

    Add guard

--- components/common/ListErrors.tsx
+++ components/common/ListErrors.tsx
@@ -1,15 +1,18 @@
 import React from "react";
 
-const ListErrors = ({ errors }) => (
-  <ul className="error-messages">
-    {Object.keys(errors).map((key) => {
-      return (
-        <li key={key}>
-          {key} {errors[key]}
-        </li>
-      );
-    })}
-  </ul>
-);
+const ListErrors = ({ errors }) => {
+  if (!errors || errors.length === 0) return null;
+  return (
+    <ul className="error-messages">
+      {Object.keys(errors).map((key) => {
+        return (
+          <li key={key}>
+            {key} {errors[key]}
+          </li>
+        );
+      })}
+    </ul>
+  );
+};
 
 export default ListErrors;
